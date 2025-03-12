6c962a688b0984e8cd54c95d513f3654cf5f5636
Wendel Silva <wendelfilipe1997@hotmail.com>
2025-03-11 23:37:14 -0300

    create graphic

--- src/pages/HomeScrollGraphic.tsx
+++ src/pages/HomeScrollGraphic.tsx
@@ -1,65 +1,59 @@
-import React from 'react';
-import { View, Text, StyleSheet, Dimensions } from 'react-native';
-import { LineChart } from 'react-native-chart-kit';
+import { format } from 'date-fns';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, Dimensions, Image } from 'react-native';
+import CalendarButton from 'src/components/CalendarButton';
+import LineChart from 'src/components/LineChart';
+import styles from 'src/styles/homeScrollGraphic';
 
 const HeartRateCard = () => {
   const data = [70, 75, 73, 78, 72, 74, 71]; // Simulação de batimentos cardíacos
   const screenWidth = Dimensions.get('window').width;
 
-  return (
-    <View style={styles.card}>
-      <Text style={styles.title}>Heart</Text>
-      <LineChart
-        data={{
-          labels: ["", "", "", "", "", "", ""],
-          datasets: [{ data }],
-        }}
-        width={screenWidth * 0.4} // Define a largura do gráfico
-        height={100}
-        chartConfig={{
-          backgroundGradientFrom: '#ffffff',
-          backgroundGradientTo: '#ffffff',
-          color: (opacity = 1) => `rgba(255, 100, 115, ${opacity})`,
-          strokeWidth: 2,
-        }}
-        bezier // Aplica suavização na linha
-        withHorizontalLines={false}
-        withVerticalLines={false}
-        style={{ marginVertical: 8 }}
-      />
-      <Text style={styles.bpm}>74</Text>
-      <Text style={styles.label}>bpm</Text>
-    </View>
-  );
-};
+  const [currentDate, setCurrentDate] = useState<string | null>(null);
+ 
+     useEffect(() => {
+         const currentDate = new Date();
+         const formatDate = format(currentDate, 'EEEE , dd MMMM')
+ 
+         setCurrentDate(formatDate)
+     }, []);
 
-const styles = StyleSheet.create({
-  card: {
-    backgroundColor: 'white',
-    padding: 16,
-    borderRadius: 16,
-    width: 140,
-    alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.1,
-    shadowRadius: 8,
-    elevation: 5,
-  },
-  title: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    color: '#333',
-  },
-  bpm: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginTop: 8,
-  },
-  label: {
-    fontSize: 14,
-    color: '#666',
-  },
-});
+  return (
+    <View style={styles.containerAll}>
+      <View style={styles.containerUser}>
+        <View style={styles.containerUserImg}>
+          <Image
+          />
+        </View>
+        <View style={styles.containerUserName}>
+          <Text style={styles.textUserWelcome}> Hello, Linh!</Text>
+          <Text style={styles.textUserDay}> {currentDate}</Text>
+        </View>
+        <View style={styles.containerUserCalendar}>
+            <CalendarButton 
+                color='black'
+            />
+        </View>
+      </View>
+      <View style={styles.containerInformation}>
+          <View style={styles.containerColunm}>
+            <View style={styles.containerCalories}>
+              <Text> Calories </Text>
+              <Text> 620.68 </Text>
+              <Text> Kcal </Text>
+            </View>
+            <View style={styles.containerSteps}>
+              <Text> Calories </Text>
+              <Text> 620.68 </Text>
+              <Text> Kcal </Text>
+            </View>
+          </View>
+          <View style={styles.containerHeart}>
+            <LineChart/>
+          </View>
+      </View>
+    </View>
+)
+}
 
 export default HeartRateCard;
