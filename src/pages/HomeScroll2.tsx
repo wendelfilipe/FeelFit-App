fa9da57ac783842d6d687af8e84bbd1179df6662
Wendel Silva <wendelfilipe1997@hotmail.com>
2025-03-25 23:15:35 -0300

    create page scroll 2

---
+++ src/pages/HomeScroll2.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const HomeScroll2: React.FC = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+} 
